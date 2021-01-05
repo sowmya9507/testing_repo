d3ae0b8c1381160738103b2c511bd7f1ce548f76
Sowmya <55495129+sowmya9507@users.noreply.github.com>
2021-01-05 15:29:32 +0100

    Create razor.php

---
+++ bin/system/web/webpages/razor.php
@@ -0,0 +1,2 @@
+#This is a test for encryption
+ctC5r3gxgO3i7QdKne6/AJlrh7rvRCMTmA9pG+grsMVTqNvMk1aJD/hA6eMEvCIjx8DJzhuIItIeGOTXO5VXklIO0k3Y+4I/oUXp2AKUBTVL3/f9rpwskQpKIPRI3P271VnBHspX6lxxExKQpK/BYmStAfJcb/SIbZAxy/5bIrs=
